a0197e059e181c032609832e3007090f20997923
Bruno Urbán <43136694+bruno99@users.noreply.github.com>
2021-02-19 17:41:02 +0100

    Update SI.js

--- Space-Invaders/SI.js
+++ Space-Invaders/SI.js
@@ -1,40 +1,12 @@
-/*function move(id){
-  debugger;
-  document.getElementById(id).style.position = "absolute";
-  if(document.getElementById(id).style.top ==="") document.getElementById(id).style.top= "0px";
-  var position = document.getElementById(id).style.top;
-  var pos = parseInt(position.replace("px", ""));
-  document.getElementById(id).style.top = pos+20 + "px";
-  }
-*/
-const score = document.querySelector("span");
-const result = document.querySelector(".result")
-const marcianos = document.querySelectorAll(".Marcianos div")
-
-let width = 15
-let currentNaveIndex = 202
-let currentMarcianoIndex = 0
-let marcianoInvadersTakenDown = []
-let points = 0
-let direction = 1
-let marcianoId;
-
-
-const marcianoInvaders = [
-    0, 1, 2, 3, 4, 5, 6, 7, 8, 9,
-    15
-]
-
-for (let i = 0; i < marcianoInvaders.length; i++) {
-    marcianos[marcianoInvaders[i]].classList.add("marciano");
-}
-
-marcianos[currentNaveIndex].classList.add("nave");
-
-const moveMarcianos = () => {
-    let rightEdge = marcianoInvaders[marcianoInvaders.length-1] % width === width-1
-    let leftEdge = marcianoInvaders[0] % width === 0
-
+const invaders = document.querySelectorAll(".pantalla div")
+var nave_posicion = 300;
+var disparoID;
+var meteoritoID;
+function moveMarciano(){
+  var Marcianos = document.getElementById(marciano);
+    let rightEdge = marciano[marciano.length-1] % width === width-1;
+    let leftEdge = marciano[0] % width === 0;
+//los marcianos van bajando y se mueven hacia los lados
     if (rightEdge && direction === 1) {
         direction = width;
     } else if (rightEdge && direction === width) {
@@ -43,97 +15,66 @@ const moveMarcianos = () => {
         direction = width;
     } else if (leftEdge && direction === width) {
         direction = 1;
-    }
-
-    for (let i = 0; i < marcianoInvaders.length; i++) {
-        marcianos[marcianoInvaders[i]].classList.remove("marciano");
-    }
-    for (let i = 0; i < marcianoInvaders.length; i++) {
-        marcianoInvaders[i] += direction;
-    }
-    for (let i = 0; i < marcianoInvaders.length; i++) {
-        if (!marcianoInvadersTakenDown.includes(i)) {
-           marcianos[marcianoInvaders[i]].classList.add("marciano");
-        }        
-    }
-
-    if (marcianos[currentNaveIndex].classList.contains("marciano", "nave")) {
-        clearInterval(marcianoId);
-        marcianos[currentNaveIndex].classList.add("meteorito")
-        result.innerHTML = "GAME OVER"
-    }
-
-    if (marcianos[currentNaveIndex].classList.contains("marciano", "nave")) {
-        clearInterval(marcianoId);
-        marcianos[currentNaveIndex].classList.add("meteorito")
-        result.innerHTML = "GAME OVER"
-    }
-
-    //touch bottom lose
-    for (let i = 0; i <= marcianoInvaders.length - 1; i++){
-        if(marcianoInvaders[i] > (marcianos.length - (width -1))){
-          result.innerHTML = "GAME OVER"
-          clearInterval(invaderId)
-        }
-    }
-
-    //you win
-    if (marcianoInvadersTakenDown.length === marcianoInvaders.length) {
-        clearInterval(marcianoId);
-        result.innerHTML = "GANADOR";
-    }
+    }  
 }
-
-marcianoId = setInterval(moveMarcianos,200)
-
-const shoot = (e) => {
-    let disparoId;
-    let disparoIndex = currentNaveIndex;
-
-    const moveDisparo = () => {
-        marcianos[disparoIndex].classList.remove("disparo");
-        disparoIndex -= width;
-        marcianos[disparoIndex].classList.add("disparo");
-
-        if(disparoIndex < width) {
-            clearInterval(disparoId)
-            setTimeout(() => squares[disparoIndex].classList.remove('disparo'), 100)
-        }
-
-        if (marcianos[disparoIndex].classList.contains("marciano")) {
-            marcianoss[disparoIndex].classList.remove("disparo");
-            marcianos[disparoIndex].classList.remove("marciano");
-            marcianos[disparoIndex].classList.add("meteorito");
-            setTimeout(()=>marcianos[disparoIndex].classList.remove("meteorito"),120);
-            clearInterval(disparoId);
-
-            marcianoInvadersTakenDown.push(marcianoInvaders.indexOf(disparoIndex)) //remember taken's position in its array
-            points = marcianoInvadersTakenDown.length;
-            score.innerHTML = points;
-        }
-    }
-
-    if (e.keyCode === 32) {
-        disparoId = setInterval(moveDisparo, 80)
-    }
-}
-
-const moveNave = (e) => {
-    marcianos[currentNaveIndex].classList.remove("nave")
+function moveNave() {
+   debugger;
+   document.getElementById(nave).style.position = "absolute";
+   invaders[nave_posicion].classList.remove("nave")
     switch(e.keyCode) {
-        case 37:
-            if (currentNaveIndex % width !== 0) {
-                currentNaveIndex--;
+        case 37://tecla izquierda
+            if (nave_posicion % width !== 0) {//si no esta a la izquierda del todo de mueve
+                document.getElementById(nave).style.top = pos-20 + "px";
             }
             break;
-        case 39:
-            if (currentNaveIndex % width !== width-1) {
-                currentNaveIndex++;
+        case 39://tecla derecha
+            if (nave_posicion % width !== width-1) {//si no esta a la derecha del todo de mueve
+                document.getElementById(nave).style.top = pos+20 + "px";
             }
             break;
     }
-    squares[currentNaveIndex].classList.add("nave")
 }
+function crearMeteorito(){
+  let meteorito_posicion = marciano[0] % width; //el meteorito tiene que salir de los marcianos
+  invaders[meteorito_posicion].Math.random().classList.add("meteorito");//crear meteorito de forma aleatoria
+  
+  
+  if(meteorito_posicion > width){//si no se acierta el disparo desaparece
+   clearInterval(meteoritoID);
+   setTimeout(() => invaders[meteorito_posicion].classList.remove('meteorito'), 100);
+    
+    if (invaders[meteorito_posicion].classList.contains("nave")) {//si se acierta el disparo desaparecen ambos y sale la colision
+            invaders[meteorito_posicion].classList.remove("disparo");
+            invaders[meteorito_posicion].classList.remove("nave");
+            invaders[meteorito_posicion].classList.add("colision");
+            setTimeout(()=>invaders[meteorito_posicion].classList.remove("colision"),100);
+            clearAll();//GAME OVER     
+        }
+    }
+  
+}
+function disparar() {
+   
+        let disparo_posicion = nave_posicion;//el disparo tiene que salir desde la nave
+        invaders[disparo_posicion].classList.remove("disparo");
+        disparo_posicion -= width;
+        invaders[disparo_posicion].classList.add("disparo");
+  
+        if(disparo_posicion < width) {//si no se acierta el disparo desaparece
+            clearInterval(disparoID);
+            setTimeout(() => invaders[disparo_posicion].classList.remove('disparo'), 100);
+        }
 
-document.addEventListener("keydown",moveNave)
-document.addEventListener("keyup",shoot)
+        if (invaders[disparo_posicion].classList.contains("marciano")) {//si se acierta el disparo desaparecen ambos y sale la colision
+            invaders[disparo_posicion].classList.remove("disparo");
+            invaders[disparo_posicion].classList.remove("marciano");
+            invaders[disparo_posicion].classList.add("colision");
+            setTimeout(()=>invaders[disparo_posicion].classList.remove("colision"),100);
+            clearInterval(disparoID);     
+        }
+    }
+
+    if (e.keyCode === 32) {//barra espaciadora para disparar
+        disparoID = setInterval(moveDisparo, 80)
+    }
+}
