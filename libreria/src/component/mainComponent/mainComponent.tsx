3c30457be12839b1aaa038f94405fc87145c9b42
Bruno Urbán <43136694+bruno99@users.noreply.github.com>
2021-04-23 00:50:30 +0200

    Add files via upload

---
+++ libreria/src/component/mainComponent/mainComponent.tsx
@@ -0,0 +1,28 @@
+import React , {FC, useState} from 'react';
+import Body from "../body/books";
+import ABody from "../body/autor"
+
+import "./style.css"
+
+const Main:FC = ()=>{
+
+    const [search,setSearch] = useState<string>("");
+    const [state,setState] =useState<string>();
+
+
+    return(
+        <div className="back">
+            <div className="title">Librería Online</div>
+            <div className= "title">Autor------------Libro</div>
+            <div className="container"> 
+                <input type="text" onChange={(e)=>setSearch(e.target.value)}/>
+                <input type="text" onChange={(e)=>setSearch(e.target.value)}/>
+                <div className="boton" onClick={(e)=>setState("searching")}>Buscar</div>
+            </div>
+            {!state&&(<div className="text_aux"></div>)}
+            {state&&<Body text={search}></Body>}
+            {state&&<ABody text={search}></ABody>}
+        </div>
+    )
+}
+export default Main;
