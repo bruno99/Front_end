452251afd69bc8050a149369852262ad540789fa
Bruno Urbán <43136694+bruno99@users.noreply.github.com>
2021-04-23 09:32:20 +0100

    Update mainComponent.tsx

--- libreria/src/component/mainComponent/mainComponent.tsx
+++ libreria/src/component/mainComponent/mainComponent.tsx
@@ -5,8 +5,8 @@ import ABody from "../body/autor"
 import "./style.css"
 
 const Main:FC = ()=>{
-
     const [search,setSearch] = useState<string>("");
+    const [searchA,setSearchA] = useState<string>("");
     const [state,setState] =useState<string>();
 
 
@@ -15,13 +15,13 @@ const Main:FC = ()=>{
             <div className="title">Librería Online</div>
             <div className= "title">Autor------------Libro</div>
             <div className="container"> 
-                <input type="text" onChange={(e)=>setSearch(e.target.value)}/>
+                <input type="text" onChange={(e)=>setSearchA(e.target.value)}/> 
                 <input type="text" onChange={(e)=>setSearch(e.target.value)}/>
                 <div className="boton" onClick={(e)=>setState("searching")}>Buscar</div>
             </div>
             {!state&&(<div className="text_aux"></div>)}
             {state&&<Body text={search}></Body>}
-            {state&&<ABody text={search}></ABody>}
+            {state&&<ABody text={searchA}></ABody>}
         </div>
     )
 }
